fb9e533f2c59ed4d14fb752883478566d3239427
Cheng Ho Ming <eric310@connect.hku.hk>
2024-03-04 23:40:14 +0800

    Account for researcher's dataset typo: column named 'Hastag' instead of 'Hashtag'

--- async.py
+++ async.py
@@ -8,11 +8,10 @@ filtered_one = []
 
 # you only need to edit the `year` variable and the `for days in range(1, 32):` loop
 # the latter is for months with 31 days, you need to change the range to 30 or 28/29 for February
-_year = "2021"
-months = "12".zfill(2)
-num_of_days = 1 + 31  # 1 + 31 because the range function is exclusive, so it will only go up to 30
+_year = "2020"
+months = "4".zfill(2)
+num_of_days = 1 + 30  # 1 + 31 because the range function is exclusive, so it will only go up to 30
 
-session = grequests.Session()
 for days in range(1, num_of_days):
     days = str(days).zfill(2)   # the file name of the csv files/folders are 2022_12_01_something, 2022_12_02_something, etc
     # so need fill the zero if the day is less than 10
@@ -28,37 +27,56 @@ for days in range(1, num_of_days):
     else:
         year = "_" + _year
 
+    if _year == "2020":
+        branch = "master"
+    else:
+        branch = "main"
+
     url = []
     for name in ["Hashtag", "Sentiment"]:
         for hour in range(24):
             hour = str(hour).zfill(2)
             if not os.path.exists(f'{date}_{hour}_Summary_{name}.csv'): # if the file does not exist, download it
-                url += [f"https://raw.githubusercontent.com/lopezbec/COVID19_Tweets_Dataset{year}/main/Summary_{name}/{folder_name}/{date}_{hour}_Summary_{name}.csv"]
+                url += [f"https://raw.githubusercontent.com/lopezbec/COVID19_Tweets_Dataset{year}/{branch}/Summary_{name}/{folder_name}/{date}_{hour}_Summary_{name}.csv"]
             #print(hour, url)
     
-    print("Downloading", date, name)
-    rs = (grequests.get(u) for u in url)
+    print("Downloading", date, "Summary Hashtag and Sentiment")
+    rs = (grequests.get(u, stream=False) for u in url)
 
     
-    results = grequests.map(rs)
+    results = grequests.map(rs, size=len(url))
     for response in results:
         if response.status_code == 200:
             with open(pyrfc6266.requests_response_to_filename(response), 'wb') as f:
                 f.write(response.content)
                 print("Downloaded", pyrfc6266.requests_response_to_filename(response))
+                response.close()
+        else:
+            print(response.status_code, response.url)
 
-
-    # Load the first CSV table with ID and Tag
-    for f in glob.glob("*_Summary_Hashtag.csv"):
-        df_tags = pd.read_csv(f)
-        print(f"Filtering {f}")
-        filtered_one += [df_tags[df_tags["Hastag"].str.contains('covid', case=False)
-                                | df_tags["Hastag"].str.contains('omicorn', case=False)
-                                | df_tags["Hastag"].str.contains('corona', case=False)
-                                | df_tags["Hastag"].str.contains('lockdown', case=False)
-                                | df_tags["Hastag"].str.contains('stayhome', case=False)
-                                | df_tags["Hastag"].str.contains('wearamask', case=False)
-                                ].drop_duplicates(subset=['Tweet_ID'])]
+    if _year != "2020":
+        # Load the first CSV table with ID and Tag
+        for f in glob.glob("*_Summary_Hashtag.csv"):
+            print(f"Filtering {f}")
+            df_tags = pd.read_csv(f, encoding= 'unicode_escape')
+            filtered_one += [df_tags[df_tags["Hastag"].str.contains('covid', case=False)
+                                    | df_tags["Hastag"].str.contains('omicorn', case=False)
+                                    | df_tags["Hastag"].str.contains('corona', case=False)
+                                    | df_tags["Hastag"].str.contains('lockdown', case=False)
+                                    | df_tags["Hastag"].str.contains('stayhome', case=False)
+                                    | df_tags["Hastag"].str.contains('wearamask', case=False)
+                                    ].drop_duplicates(subset=['Tweet_ID'])]
+    else:   # Researcher data mistake typo :(((((
+        for f in glob.glob("*_Summary_Hashtag.csv"):
+            print(f"Filtering {f}")
+            df_tags = pd.read_csv(f, encoding= 'unicode_escape')
+            filtered_one += [df_tags[df_tags["Hashtag"].str.contains('covid', case=False)
+                                    | df_tags["Hashtag"].str.contains('omicorn', case=False)
+                                    | df_tags["Hashtag"].str.contains('corona', case=False)
+                                    | df_tags["Hashtag"].str.contains('lockdown', case=False)
+                                    | df_tags["Hashtag"].str.contains('stayhome', case=False)
+                                    | df_tags["Hashtag"].str.contains('wearamask', case=False)
+                                    ].drop_duplicates(subset=['Tweet_ID'])]
 
     merged_df = pd.DataFrame()
     if len(filtered_one) >= 2:
